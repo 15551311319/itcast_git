c4ddf824278516d899196356499d6947f83bbeb6
itcast <bas@itcast.cn>
2019-07-09 11:38:05 +0800

    项目的其他部分提交到本地仓库

---
+++ day69_git/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,16 @@
+package com.itheima.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @ClassName: User
+ * @Description: TODO
+ * @Author: lf bai
+ * @Date: 2019/07/09 11:22
+ * @Version: v1.0
+ */
+public class User {
+    private String username;
+    private Integer age;
+
+}
